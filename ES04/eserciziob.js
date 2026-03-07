de383d0d04357e05e1df956fe3e13021f0a11286
Aurora Pluderi <auryplude1606@gmail.com>
2026-03-07 11:24:12 +0000

    inserimento esercizio b

---
+++ ES04/eserciziob.js
@@ -0,0 +1,4 @@
+// Esercizio b: Conteggio decrescente da 100 a 0 con passo 20
+for (let i = 100; i >= 0; i -= 20) {
+    console.log(i);Ad ogni giro del ciclo, "dice" al terminale di scrivere il valore attuale di i
+}
